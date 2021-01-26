a099cac27d1fce490f963b34c4a788d95d5c4b04
tisrar <67045159+tisrar@users.noreply.github.com>
2021-01-26 13:40:54 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var cat, catImage2, catImage, catImage3, catImage4; 
+var mouse, mouseImage1, mouseImage2, mouseImage3, mouseImage4; 
+var background,backgroundImage
+function preload() {
+    //load the images here
+catImage=loadImage("cat1.png")
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background("red");
+    //Write condition here to evalute if tom and jerry collide
+if (keyCode === LEFT_ARROW){
+
+    
+}
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
